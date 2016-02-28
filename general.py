4802301ff1d7d18bb7b26fac3c836f7602dc8226
Aditya Giridhar <agiridh@purdue.edu>
2016-02-28 15:20:54 -0500

    wrote code to create queue.txt and crawled.txt

--- general.py
+++ general.py
@@ -5,4 +5,19 @@ def create_new_dir(directory):
         print("Creating new project {}...".format(directory))
         os.makedirs(directory)
 
-create_new_dir("google")
+# create new file
+def create_file(file, data):
+    with open(file, 'w') as f:
+        f.write(data)
+
+# create queue and crawled files
+# queue: consists of pages that have not been crawled
+# crawled: consists of pages that have already been crawled
+def create_data_files(project_name, base_url):
+    queue = project_name + "queue.txt"
+    crawled = project_name + "crawled.txt"
+    if not os.path.isfile(queue):
+        create_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        create_file(crawled, base_url)
+
