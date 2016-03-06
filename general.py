b7be1232b420e67b35dc3c403a6d038517e51631
Aditya Giridhar <agiridh@purdue.edu>
2016-03-06 14:36:20 -0500

    reducing time spent on File I/O

--- general.py
+++ general.py
@@ -27,10 +27,28 @@ def create_data_files(project_name, base_url):
 # Add another link to existing file
 def append_data_to_file(path, data):
     with open(path, 'a') as file:
-        file.write(data)
+        file.write(data + '\n')
 
 
 # write nothing onto file = deleting contents of file
 def delete_file_content(path):
-    with open(path) as file:
+    with open(path, 'w'):
         pass
+
+# File I/O is a bottleneck in a program that crawls through thousands
+# of web pages. So, it is better to write those crawled website links to a set
+# this function stores each line of file in a set
+def add_filelines_to_set(file):
+    with open(file, 'rt') as f:
+        results = set()
+        for line in file:
+            results.add(line.replace("\n", ""))
+
+    return results
+
+
+# each item of set will be a new line in the file
+def writing_set_to_file(links, file):
+    delete_file_content(file)
+    for link in sorted(links):
+        append_data_to_file(file, link)
