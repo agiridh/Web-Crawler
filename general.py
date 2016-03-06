2140751c8d0dad548fdc77ec9b930bf8da4c74f7
Aditya Giridhar <agiridh@purdue.edu>
2016-03-06 14:16:19 -0500

    wrote functions to add and delete links

--- general.py
+++ general.py
@@ -5,10 +5,12 @@ def create_new_dir(directory):
         print("Creating new project {}...".format(directory))
         os.makedirs(directory)
 
+
 # create new file
 def create_file(file, data):
     with open(file, 'w') as f:
         f.write(data)
+
 
 # create queue and crawled files
 # queue: consists of pages that have not been crawled
@@ -21,3 +23,14 @@ def create_data_files(project_name, base_url):
     if not os.path.isfile(crawled):
         create_file(crawled, base_url)
 
+
+# Add another link to existing file
+def append_data_to_file(path, data):
+    with open(path, 'a') as file:
+        file.write(data)
+
+
+# write nothing onto file = deleting contents of file
+def delete_file_content(path):
+    with open(path) as file:
+        pass
